d19efe646581d932be2e92da69d3ba3343e7d3c3
Alexey Karpovich <drflemus@gmail.com>
2025-06-14 04:55:31 +0300

    Second commit with O(n) sorting instead of bubble-sort

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,53 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
         File file = new File("Romeo and Juliet.txt");
-        String text = "";
+        StringBuilder builder = new StringBuilder();
+
         try {
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            while(fileReader.ready()) {
-                text += (char)fileReader.read();
-            }
+            String line;
+            do {
+                line = bufferedReader.readLine();
+                builder.append(line).append("\n");
+            } while(line!=null);
         }
          catch (IOException e) {
             throw new RuntimeException(e);
         }
-//        System.out.println(text);
-        String regex = "[!\"#$%&()*+,-./:;<=>?@\\[\\\\\\]^_`{|}~\\s]";
-        String[] split = text.split(regex);
 
-        for(int i = 0; i< split.length; i++){
-            for (int j = 1; j < split.length-1; j++) {
-                String temp = split[i];
-                if(split[j].length()<split[i].length()){
-                    split[i] = split[j];
-                    split[j] = temp;
-                }
+        String text = builder.toString();
+
+//        System.out.println(text);
+
+        String regex = "[!\"#$%&()*+,-./:;<=>?@\\[\\\\\\]^_`{|}~\\s]";
+        String[] words = text.split(regex);
+
+        String longestWord = "";
+
+        for(String word : words){
+            if(!word.isEmpty() && longestWord.length()<word.length()){
+                longestWord = word;
             }
         }
-        //split[1]
-        File newFile = new File("biggest word.txt");
-        try{
-            file.createNewFile();
-            FileWriter fw = new FileWriter(newFile);
-            fw.append(split[1]);
-            fw.close();
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
+
+        System.out.println(longestWord);
+
+//      words[1]
+
+//        File newFile = new File("biggest word.txt");
+//        try{
+//            file.createNewFile();
+//            FileWriter fw = new FileWriter(newFile);
+//            fw.append(words[1]);
+//            fw.close();
+//        }
+//        catch (IOException e){
+//            e.printStackTrace();
+//        }
 
     }
 }
