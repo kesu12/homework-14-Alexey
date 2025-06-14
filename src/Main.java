4c501a4af9255ffbb20dd83a0220499c066c3eb4
Alexey Karpovich <drflemus@gmail.com>
2025-06-14 04:36:43 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        File file = new File("Romeo and Juliet.txt");
+        String text = "";
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while(fileReader.ready()) {
+                text += (char)fileReader.read();
+            }
         }
+         catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+//        System.out.println(text);
+        String regex = "[!\"#$%&()*+,-./:;<=>?@\\[\\\\\\]^_`{|}~\\s]";
+        String[] split = text.split(regex);
+
+        for(int i = 0; i< split.length; i++){
+            for (int j = 1; j < split.length-1; j++) {
+                String temp = split[i];
+                if(split[j].length()<split[i].length()){
+                    split[i] = split[j];
+                    split[j] = temp;
+                }
+            }
+        }
+        //split[1]
+        File newFile = new File("biggest word.txt");
+        try{
+            file.createNewFile();
+            FileWriter fw = new FileWriter(newFile);
+            fw.append(split[1]);
+            fw.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
